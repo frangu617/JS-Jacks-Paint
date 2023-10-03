f7b429caf0351c589c84b9a2df3913f2cdc21d1c
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-10-03 16:10:40 -0700

    started

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -30,31 +30,49 @@ function getProductInfo(paintColor) {
     
     switch (paintColor) {
         case 'pn1':           
-            // set variables for price and color name and invoke a function to update the price     
+            // set variables for price and color name and invoke a function to update the price 
+                 price = "$14.99";
+                 colorName = "Lime green";
             break;           
         case 'pn2':
-            // set variables for price and color name and invoke a function to update the price    
+            // set variables for price and color name and invoke a function to update the price
+             price = "$11.99";
+             colorName = "Brown";    
             break;            
         case 'pn3':
             // set variables for price and color name and invoke a function to update the price  
+             price = "$22.99";
+             colorName = "Royal blue";
             break;   
         case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
+            // set variables for price and color name and invoke a function to update the price 
+            price = "$4.99";
+            colorName = "Solid Black"; 
             break;   
         case 'pn5':
-            // set variables for price and color name and invoke a function to update the price       
+            // set variables for price and color name and invoke a function to update the price
+            price = "$8.22";
+            colorName = "Solid Cyan";        
             break;   
         case 'pn6':
-            // set variables for price and color name and invoke a function to update the price        
+            // set variables for price and color name and invoke a function to update the price  
+            price = "$11.99";
+            colorName = "Solid Purple";       
             break;   
         case 'pn7':
             // set variables for price and color name and invoke a function to update the price 
+            price = "$13.42";
+            colorName = "Solid Red"; 
             break;   
         case 'pn8':
             // set variables for price and color name and invoke a function to update the price   
+            price = "$21.98";
+            colorName = "Solid White"; 
             break;   
         case 'pn9':
             // set variables for price and color name and invoke a function to update the price 
+            price = "$14.99";
+            colorName = "Solid Yellow"; 
             break;   
           default:              
     }
